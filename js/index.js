69dcd94aa6246cc6324eef545c919d99eea8cf11
rajansth34 <rajansth34@gmail.com>
2024-02-23 21:04:09 -0700

    Header font-size change

--- js/index.js
+++ js/index.js
@@ -2,16 +2,17 @@ const hamburger = document.querySelector("button#hamburger")
 const close = document.querySelector("button#close")
 const list = document.querySelector("ul.list")
 
-function displayMenu(){
-     hamburger.style.display="none"
-     close.style.display="block"
+
+function displayMenu() {
+     hamburger.style.display = "none"
+     close.style.display = "block"
      list.style.display = "block";
-    
+
 }
 
-function closeMenu(){
-    console.log("display!");
-    hamburger.style.display="block"
-    close.style.display="none"
-   list.style.display = "none";
-}
+function closeMenu() {
+     console.log("display!");
+     hamburger.style.display = "block"
+     close.style.display = "none"
+     list.style.display = "none";
+} 
